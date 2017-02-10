a2b79c100d781aa52b97653ab837914242c64fb9
Samsil Arefin <samsilarefin000@gmail.com>
2017-02-10 22:22:33 +0600

    Add files via upload

---
+++ 10696.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<cmath>
+#include<cstdlib>
+#include<cstdio>
+using namespace std;
+
+ 
+
+int main () {
+  unsigned long long m;
+   while(cin>>m && m)
+   {
+   	 if(m>=101)cout<<"f91("<<m<<") = "<<m-10<<endl;
+   	 
+   	 else cout<<"f91("<<m<<") = 91"<<endl;
+   }
+     
+    
+    
+
+   return 0;
+}
