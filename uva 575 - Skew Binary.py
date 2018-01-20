5fd81800c1331af369ecaae851c0406cd7c508d9
Samsil Arefin <samsilarefin000@gmail.com>
2018-01-21 04:18:55 +0600

    Add files via upload

---
+++ uva 575 - Skew Binary.py
@@ -0,0 +1,17 @@
+while True:
+    s = (input()) #skew  binary input
+    if s=='0' :
+        break #when input =0 program will be terminated
+
+    else :
+
+        s = s[::-1] #reverse integers
+        res = 0
+        for i, n in enumerate(s):
+            res += int(n) * ((2 ** (i + 1)) - 1) #(2**(n+1))-1 is the formula of skew binary
+
+        print(res) #print result
+
+
+
+
